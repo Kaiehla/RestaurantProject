d91f7ebfa67ca96f8a3cac55a20a5fd579abf7cc
Kai <espiritu.kaiehla@gmail.com>
2023-11-09 21:14:07 +0800

    added a message

--- RestaurantProject/Controllers/HomeController.cs
+++ RestaurantProject/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace RestaurantProject.Controllers
 {
-    public class HomeController : Controller //KAR WAS HERE HI KAI 
+    public class HomeController : Controller //KAR WAS HERE HI KAI hellooo karylle
     {
         private readonly ILogger<HomeController> _logger;
 
