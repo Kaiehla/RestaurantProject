b30eac563317b998c219ce81a8ab22c29b40a53a
Karylle Legaspino <karylle.legaspino.cics@ust.edu.ph>
2023-11-10 18:08:40 +0800

    created a PackageView.cshtml, updated navs

--- RestaurantProject/Controllers/HomeController.cs
+++ RestaurantProject/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace RestaurantProject.Controllers
             return View();
         }
 
+        public IActionResult PackageView()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
