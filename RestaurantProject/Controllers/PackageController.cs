6a8b3d4fd648a96e051f731562c141ad64c56f5c
Cyrus Olega <cyrusabraham.olega.cics@ust.esu.ph>
2023-11-11 17:24:24 +0800

    - Moved PackageView to sepearate folder - Added PackageMenu data services - Added Package Controller

---
+++ RestaurantProject/Controllers/PackageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantProject.Models;
+using RestaurantProject.Services;
+
+namespace RestaurantProject.Controllers
+{
+    public class PackageController : Controller
+    {
+        private IPackageMenuDataService _packageMenu;
+
+        public PackageController(IPackageMenuDataService packageMenu)
+        {
+            _packageMenu = packageMenu;
+        }
+
+        public async Task<IActionResult> PackageView()
+        {
+            var packageMenu = await _packageMenu.GetPackage_MenusAsync();
+            return View(packageMenu);
+        }
+
+    }
+}
---
+++ RestaurantProject/Services/IPackageMenuDataService.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantProject.Models;
+
+namespace RestaurantProject.Services
+{
+    public interface IPackageMenuDataService
+    {
+        Task<List<Package_Menu>> GetPackage_MenusAsync();
+        Task<List<Package_Menu>> AddPackage_MenuAsync(Package_Menu packageMenu);
+        Task<List<Package_Menu>> UpdatePackage_MenuAsync(Package_Menu packageMenu);
+        Task<List<Package_Menu>> DeletePackage_MenuAsync(Package_Menu packageMenu);
+    }
+}
